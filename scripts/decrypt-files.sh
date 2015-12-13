4a18f5391800cb6f6fb2fd6ac676440ef1484e81
Evan Borysko <eborysko@g.harvard.edu>
2015-12-12 19:43:04 -0800

    [SKCP-108] Added logging

--- scripts/decrypt-files.sh
+++ scripts/decrypt-files.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+echo "=== Encrypted Files ==="
+ls -lt ../scripts/certs
+ls -lt ../scripts/profiles
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in '../scripts/profiles/educscie71immapp_AdHoc.mobileprovision.enc' -d -a -out '../scripts/profiles/educscie71immapp_AdHoc.mobileprovision'
 #openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in ../scripts/certs/apple.cer.enc -d -a -out ../scripts/certs/apple.cer
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in ../scripts/certs/ios_development.cer.enc -d -a -out ../scripts/certs/ios_development.cer
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in ../scripts/certs/ios_distribution.cer.enc -d -a -out ../scripts/certs/ios_distribution.cer
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in ../scripts/certs/ios_distribution.p12.enc -d -a -out ../scripts/certs/ios_distribution.p12
+echo "=== Decrypted Files ==="
+ls -lt ../scripts/certs
+ls -lt ../scripts/profiles
